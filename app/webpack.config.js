6728a8da3c33a2c1d615a41bccbf691c54051f47
lsands5400 <lsands@g.hmc.edu>
2025-11-22 16:50:27 -0800

    Added web login page

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,7 +1,12 @@
 const { getDefaultConfig } = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
-  const config = await getDefaultConfig({ ...env, babel: { dangerouslyAddModulePathsToTranspile: ['expo-router'] } });
+  const config = await getDefaultConfig({ ...env, 
+    babel: 
+    { dangerouslyAddModulePathsToTranspile: 
+      ['expo-router',
+        '@react-native-async-storage/async-storage',] 
+      } });
 
   // Fix for Cloudflare / browser environment
   config.resolve.fallback = {
